81fc7b6de9ab84f1ad9080d7bd4967171ae32674
LeandrePiggott <89439172+LeandrePiggott@users.noreply.github.com>
2021-10-29 13:32:22 +0930

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,114 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import { useState } from 'react';
 
-export default function App() {
+
+function JustifyContentBasics() {
+  const [justifyContent, setJustifyContent] = useState("flex-start");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PreviewLayout
+      label="NeighborAssist            Fannie Bay"
+      selectedValue={justifyContent}
+      values={[
+        "Log in",
+        "Register",
+        "SOS",
+        "Assistance",
+      ]}
+      setSelectedValue={setJustifyContent}
+    >
+      <View
+        style={[styles.box, { backgroundColor: "powderblue" }]} />
+      <View
+        style={[styles.box, { backgroundColor: "skyblue" }]} />
+      <View
+        style={[styles.box, { backgroundColor: "steelblue" }]} />
+    </PreviewLayout>
   );
 }
 
+const PreviewLayout = ({
+  label,
+  children,
+  values,
+  selectedValue,
+  setSelectedValue,
+}) => (
+  <View style={{ padding: 10, flex: 1 }}>
+    <Text style={styles.label}>{label}</Text>
+    <View style={styles.row}>
+      {values.map((value) => (
+        <TouchableOpacity
+          key={value}
+          onPress={() => setSelectedValue(value)}
+          style={[styles.button, selectedValue === value && styles.selected]}
+        >
+          <Text
+            style={[
+              styles.buttonLabel,
+              selectedValue === value && styles.selectedLabel,
+            ]}
+          >
+            {value}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+    <View style={[styles.container, { [label]: selectedValue }]}>
+      {children}
+    </View>
+  </View>
+);
+
+
+  
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+  marginTop: 50,
+  flex: 1, backgroundColor: "#0D98BA",
+  alignItems: 'center',
+},
+box: {
+  width: 365,
+  height: 188,
+},
+row: {
+  flexDirection: "row",
+  flexWrap: "wrap",
+},
+button: {
+  paddingHorizontal: 8,
+  paddingVertical: 6,
+  borderRadius: 4,
+  backgroundColor: "skyblue",
+  alignSelf: "flex-start",
+  marginHorizontal: "1%",
+  marginBottom: 6,
+  minWidth: "48%",
+  textAlign: "center",
+},
+selected: {
+  backgroundColor: "#0D98BA",
+  borderWidth: 0,
+},
+buttonLabel: {
+  fontSize: 14,
+  fontWeight: "800",
+  color: "black",
+  textAlign: "center"
+
+},
+selectedLabel: {
+  color: "white",
+},
+label: {
+  textAlign: "center",
+  marginBottom: 10,
+  fontSize: 24,
+},
 });
+
+
+export default JustifyContentBasics;
